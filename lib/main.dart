eb327fe99e0d2717bd1d885e0df2d91a3a41b90e
pmhweb <phyomyathein456@gmail.com>
2020-12-04 13:52:05 +0630

    version 1

---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundColor: Colors.green,
+              radius: 50.0,
+              backgroundImage: AssetImage('images/img2.JPG'),
+            ),
+            Text(
+              'PMH web',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              'Web developer',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                letterSpacing: 2.5,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 100,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    '+123456789',
+                    style: TextStyle(
+                      color: Colors.teal[900],
+                    ),
+                  ),
+                )),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(vertical: 0, horizontal: 20),
+              //padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: ListTile(
+                leading: Icon(Icons.email, color: Colors.teal),
+                title: Text(
+                  'phyomyat@gmail.com',
+                  style: TextStyle(
+                    color: Colors.teal[900],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+      ),
+    ),
+  );
+}
+
+// Row(
+//     children: <Widget>[
+//       Icon(Icons.email, color: Colors.teal),
+//       SizedBox(
+//         width: 20,
+//       ),
+//       Text(
+//         'phyomyat@gmail.com',
+//         style: TextStyle(
+//           color: Colors.teal[900],
+//         ),
+//       ),
+//     ],
+//   ),
+
+// Row(
+//     children: <Widget>[
+//       Icon(Icons.phone, color: Colors.teal),
+//       SizedBox(
+//         width: 20,
+//       ),
+//       Text(
+//         '+123456789',
+//         style: TextStyle(
+//           color: Colors.teal[900],
+//         ),
+//       ),
+//     ],
+//   ),
